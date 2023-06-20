55f26c214c2676c2ff1108e4e22603df76791f33
zbari023 <barry450643@gmail.com>
2023-06-20 14:54:14 +0200

    new in db

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here, post erstellen, bd designing, bei eine zugefügte in models muss folgendes gemacht werden: python manage.py makemigrations ; python manage.py migrate
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    Content = models.TextField(max_length=15000)
+    
+    
+    
