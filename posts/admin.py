787b6d0cabd484dc4d565b5087e883d3e43a2303
zbari023 <barry450643@gmail.com>
2023-06-20 15:56:43 +0200

    new class for Author

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Author
 admin.site.register(Post)   # post in admin seite anmelden
+admin.site.register(Author) 
--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 # Create your models here, post erstellen, bd designing, bei eine zugefügte in models muss folgendes gemacht werden: python manage.py makemigrations ; python manage.py migrate
+# ; python manage.py runserver
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(max_length=500)
+
+
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
