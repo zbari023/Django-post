774c8833ffa5677d5cc08cfe740a57a2c628a019
zbari023 <barry450643@gmail.com>
2023-06-20 18:40:44 +0200

    tag library from django

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from taggit.managers import TaggableManager      # -----------
 
 # Create your models here, post erstellen, bd designing, bei eine zugefügte in models muss folgendes gemacht werden: python manage.py makemigrations ; python manage.py migrate
 # ; python manage.py runserver
@@ -6,7 +7,8 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
     bio = models.TextField(max_length=500)
-    def __str__(self):                         # um in adminpanel den name zu ändern
+    image = models.ImageField(upload_to='Author')
+    def __str__(self):                                # um in adminpanel den name zu ändern
         return self.name
 
 
@@ -17,6 +19,8 @@ class Post(models.Model):
     date = models.DateTimeField()
     Content = models.TextField(max_length=15000)
     author = models.ForeignKey(Author,related_name='post_Author',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='Post')
+    tags = TaggableManager()
     def __str__(self):
         return self.title
     
