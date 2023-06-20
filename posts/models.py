b679b4bac595f3749df98719b65703d7664bf9ab
zbari023 <barry450643@gmail.com>
2023-06-20 23:32:04 +0200

    new add in code

--- posts/models.py
+++ posts/models.py
@@ -23,6 +23,3 @@ class Post(models.Model):
     tags = TaggableManager()
     def __str__(self):
         return self.title
-    
-    
-    
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+# Create your views
+def post_list(request):
+    data = Post.objects.all()
+    return render(request,'posts/index.html',{'posts':data})
