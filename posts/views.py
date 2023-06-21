08d80c38f39cf247cf2a69aa06c9c49679984208
zbari023 <barry450643@gmail.com>
2023-06-21 19:23:25 +0200

    date from frontend to db

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 # Create your views
 def post_list(request):
@@ -9,3 +10,13 @@ def post_list(request):
 def post_detail(request,post_id):
     data = Post.objects.get(id=post_id)
     return render(request,'posts/detail.html',{'posts':data})
+
+def new_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = PostForm()
+    
+    return render(request,'posts/new.html',{'form':form})
