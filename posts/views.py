84b160fc81980fd806b332f44e66772d69f3330c
zbari023 <barry450643@gmail.com>
2023-06-21 18:22:35 +0200

    post_detail and image

--- posts/views.py
+++ posts/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     data = Post.objects.all()
     return render(request,'posts/index.html',{'posts':data})
+
+def post_detail(request,post_id):
+    data = Post.objects.get(id=post_id)
+    return render(request,'posts/detail.html',{'posts':data})
--- project/settings.py
+++ project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/Sstatic/'             # --------------
+STATIC_URL = '/static/'             # --------------
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
